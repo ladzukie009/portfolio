a98998e3f107357633571751bd22d698d5c935bf
Jerome Espiritu <jeromeespiritu93@gmail.com>
2024-06-21 16:20:27 +0800

    Fix email services

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,17 +1,17 @@
-import { useState, useRef } from 'react';
-import { motion } from 'framer-motion';
-import emailjs from '@emailjs/browser';
-import { styles } from '../styles';
-import { SectionWrapper } from '../hoc';
-import { slideIn } from '../utils/motion';
-import { send, sendHover } from '../assets';
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { styles } from "../styles";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
+import { send, sendHover } from "../assets";
 
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
-    name: '',
-    email: '',
-    message: '',
+    name: "",
+    email: "",
+    message: "",
   });
   const [loading, setLoading] = useState(false);
 
@@ -29,32 +29,32 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'service_lhf7ar3', // paste your ServiceID here (you'll get one when your service is created).
-        'template_us5jk7c', // paste your TemplateID here (you'll find it under email templates).
+        "service_lhf7ar3", // paste your ServiceID here (you'll get one when your service is created).
+        "template_us5jk7c", // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'Jerome', // put your name here.
+          to_name: "Jerome", // put your name here.
           from_email: form.email,
-          to_email: 'jeromeespiritu93@gmail.com', //put your email here.
+          to_email: "jeromeespiritu93@gmail.com", //put your email here.
           message: form.message,
         },
-        '85UoMruw7HyeUqVfq' //paste your Public Key here. You'll get it in your profile section.
+        "Ittt0cYUVPGO0rLcE" //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
           setLoading(false);
-          alert('Thank you. I will get back to you as soon as possible.');
+          alert("Thank you. I will get back to you as soon as possible.");
 
           setForm({
-            name: '',
-            email: '',
-            message: '',
+            name: "",
+            email: "",
+            message: "",
           });
         },
         (error) => {
           setLoading(false);
           console.log(error);
-          alert('Something went wrong. Please try again.');
+          alert("Something went wrong. Please try again.");
         }
       );
   };
@@ -62,17 +62,20 @@ const Contact = () => {
   return (
     <div
       className="-mt-[8rem] xl:flex-row flex-col-reverse 
-      flex gap-10 overflow-hidden">
+      flex gap-10 overflow-hidden"
+    >
       <motion.div
-        variants={slideIn('left', 'tween', 0.2, 1)}
-        className="flex-[0.75] bg-jet p-8 rounded-2xl">
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-jet p-8 rounded-2xl"
+      >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadTextLight}>Contact.</h3>
 
         <form
           ref={formRef}
           onSubmit={handleSubmit}
-          className="mt-10 flex flex-col gap-6 font-poppins">
+          className="mt-10 flex flex-col gap-6 font-poppins"
+        >
           <label className="flex flex-col">
             <span className="text-timberWolf font-medium mb-4">Your Name</span>
             <input
@@ -132,13 +135,14 @@ const Contact = () => {
             transition duration-[0.2s] ease-in-out"
             onMouseOver={() => {
               document
-                .querySelector('.contact-btn')
-                .setAttribute('src', sendHover);
+                .querySelector(".contact-btn")
+                .setAttribute("src", sendHover);
             }}
             onMouseOut={() => {
-              document.querySelector('.contact-btn').setAttribute('src', send);
-            }}>
-            {loading ? 'Sending' : 'Send'}
+              document.querySelector(".contact-btn").setAttribute("src", send);
+            }}
+          >
+            {loading ? "Sending" : "Send"}
             <img
               src={send}
               alt="send"
@@ -152,4 +156,4 @@ const Contact = () => {
   );
 };
 
-export default SectionWrapper(Contact, 'contact');
+export default SectionWrapper(Contact, "contact");
